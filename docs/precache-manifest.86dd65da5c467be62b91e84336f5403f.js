7aac5ccbdd60a98f99e8903c97c0acd223072978
Andres Quinto Quinto <andres.quinto@sofka.com.co>
2020-07-22 19:36:19 -0500

    Second commit

---
+++ docs/precache-manifest.86dd65da5c467be62b91e84336f5403f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5f03050f9893b18ae901e1a31addb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00b55aec3ef9e2cf1231",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00b55aec3ef9e2cf1231",
+    "url": "/static/js/main.00ab3ee0.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
